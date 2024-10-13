fcb6f556a5ffd51660dbec518d35ff4a5a093c39
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-13 15:20:14 -0300

    another function

--- CO2.py
+++ CO2.py
@@ -20,7 +20,8 @@ def main():
         inquirer.List('services', message="O que gostaria de calcular hoje?", 
                       
                       choices=["Emissão de CO2 para Gasolina", 
-                               "Emissão de CO2 para Etanol", 
+                               "Emissão de CO2 para Etanol (anidro)",
+                               "Emissão de CO2 para Etanol (hidratado)", 
                                "Emissão de CO2 para Diesel", 
                                "Emissão de CO2 para Bio Diesel", 
                                "Emissão de CO2 para Eletricidade"]),
@@ -55,8 +56,32 @@ def main():
             
         emissao_gasolina()
         
-    if selected_service['services'] == "Emissão de CO2 para Etanol":
-        print(f"{username['name']} escolheu calcular a emissão de CO2 para Etanol.")
+    if selected_service['services'] == "Emissão de CO2 para Etanol (anidro)":
+        def emissao_etanol_anidro():  
+      
+            try:   
+        
+                distancia = int(input("Qual a distância percorrida em km? "))
+        
+                consumo = int(input("Quantos litros foram utilizados? "))
+        
+                con = consumo / 100  
+        
+                litros_usados = (distancia / 100) * con 
+        
+                fator_emissao = 2.716/litros_usados
+        
+                co2_emitido = litros_usados * fator_emissao  
+        
+                print(f"A emissão total de carbono é: {co2_emitido:.2f} kg CO₂") 
+        
+            except ValueError:
+        
+                print("Por favor, insira valores numéricos válidos.")
+                
+        emissao_etanol_anidro()
+        
+    if selected_service['services'] == "Emissão de CO2 para Etanol (hidratado)":
         
     if selected_service['services'] == "Emissão de CO2 para Diesel":
         print(f"{username['name']} escolheu calcular a emissão de CO2 para Diesel.")
