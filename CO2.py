670a7ec0d5df764bc77ee57513a8214ab1810dc6
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-14 09:37:08 -0300

    inserting the functions inside a class

--- CO2.py
+++ CO2.py
@@ -2,56 +2,71 @@ import functions
 
 import inquirer
 
+import asyncio
 
-def main():
-    nameQuestion = [
-        inquirer.Text('name', message="Qual é o seu nome?"),
-    ]
+class main: 
     
-    username = inquirer.prompt(nameQuestion)
-    
-    if username['name'] == "":
-        print("Por favor, digite seu nome.")
-        return main() 
-    
-    print(f"Olá, {username['name']}! Seja bem-vindo(a).")
-
-    servicesQuestionList = [
-        inquirer.List('services', message="O que gostaria de calcular hoje?", 
-                      choices=["Emissão de CO2 para Gasolina", 
-                               "Emissão de CO2 para Etanol (anidro)",
-                               "Emissão de CO2 para Etanol (hidratado)", 
-                               "Emissão de CO2 para Diesel", 
-                               "Emissão de CO2 para Bio Diesel", 
-                               "Emissão de CO2 para Eletricidade"]),
-    ]
-    
-    selected_service = inquirer.prompt(servicesQuestionList)
-    
-    if selected_service['services'] == "Emissão de CO2 para Gasolina":
+    async def start ():
         
-        functions.emissao_gasolina()
-      
-    if selected_service['services'] == "Emissão de CO2 para Etanol (anidro)":
-
-       functions.emissao_etanol_anidro()
-       
-    if selected_service['services'] == "Emissão de CO2 para Etanol (hidratado)":
+        nameQuestion = [
+            
+            inquirer.Text('name', message="Qual é o seu nome?"),
+            
+        ]
         
-        functions.emissao_etanol_hidratado()
-
-    if selected_service['services'] == "Emissão de CO2 para Diesel":
-
-        functions.emissao_diesel()
-
-    if selected_service['services'] == "Emissão de CO2 para Bio Diesel":
+        username = inquirer.prompt(nameQuestion);
         
-        functions.emissao_bio_diesel()
+        if username['name'] == "":
+            
+            print("Por favor, digite seu nome.")
+            
+            return main.start()  # Chama novamente a função se o nome estiver vazio.
+        
+        print(f"Olá, {username['name']}! Seja bem-vindo(a).")
+        
 
-    if selected_service['services'] == "Emissão de CO2 para Eletricidade":
-
-        functions.calcular_emissao_eletrica()
+    async def allChoices ():
+        
+        await main.start()
+        
+        servicesQuestionList = [
+            
+            inquirer.List('services', message="O que gostaria de calcular hoje?", 
+                          choices=["Emissão de CO2 para Gasolina", 
+                                   "Emissão de CO2 para Etanol (anidro)",
+                                   "Emissão de CO2 para Etanol (hidratado)", 
+                                   "Emissão de CO2 para Diesel", 
+                                   "Emissão de CO2 para Bio Diesel", 
+                                   "Emissão de CO2 para Eletricidade"]),
+            
+        ]
+        
+        selected_service = inquirer.prompt(servicesQuestionList);
+        
+        if selected_service['services'] == "Emissão de CO2 para Gasolina":
+            
+            functions.emissao_gasolina()
+            
+        if selected_service['services'] == "Emissão de CO2 para Etanol (anidro)":
+            
+            functions.emissao_etanol_anidro()
+            
+        if selected_service['services'] == "Emissão de CO2 para Etanol (hidratado)":
+        
+            functions.emissao_etanol_hidratado()
+            
+        if selected_service['services'] == "Emissão de CO2 para Diesel":
+            
+            functions.emissao_diesel()
+            
+        if selected_service['services'] == "Emissão de CO2 para Bio Diesel":
+            
+            functions.emissao_bio_diesel()
+            
+        if selected_service['services'] == "Emissão de CO2 para Eletricidade":
+            
+            functions.calcular_emissao_eletrica()
         
 if __name__ == "__main__":
     
-    main()
+    asyncio.run(main.allChoices())
